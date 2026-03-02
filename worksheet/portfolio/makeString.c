47911531ed9d2e87a9eee070cc29e5481c8421c4
Refiloe M. Mosiako <fmxr7642@leeds.ac.uk>
2026-03-02 13:08:22 +0000

    worksheet 4

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -10,8 +10,27 @@ Student ID: 201834606
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[51] = "";
 
-    // process the command-line data using appropriate string functions
+    // process the command-line data using appropriate string functions  
+    if (argc < 2 || argc > 11) {
+        strcpy(buffer, "Error: Invalid number of arguments");
+    }
+    else {
+        for (int i = 1; i < argc; i++) {
+
+            if (strlen(argv[i]) < 1 || strlen(argv[i]) > 4) {
+                strcpy(buffer, "Error: Invalid argument length");
+                break;
+            }
+
+            strcat(buffer, argv[i]);
+
+            if (i < argc - 1) {
+                strcat(buffer, "-");
+            }
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
